42be272f0321319f5fa7b2c9f409c14f5d644434
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-09-27 11:24:33 -0300

    protecting routes

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,38 +2,39 @@ const asyncHanlder = require("express-async-handler");
 const Contact = require("../models/contactModel");
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access Public
+//@access private
 const getContacts = asyncHanlder(async (req, res) => {
-  const contact = await Contact.find();
+  const contact = await Contact.find({ user_id: req.user.id });
   res.status(200).json(contact);
 });
 
 //@desc Create new contact
 //@route POST /api/contacts
-//@access Public
+//@access private
 const createContacts = asyncHanlder(async (req, res) => {
   console.log(req.body);
   const { name, email, phone } = req.body;
-  if(!name || !email || !phone) {
+  if (!name || !email || !phone) {
     res.status(400);
-    throw new Error("All filed are mandatory!")
+    throw new Error("All filed are mandatory!");
   }
 
   const contact = await Contact.create({
-    name, 
+    name,
     email,
     phone,
-  })
+    user_id: req.user.id
+  });
 
   res.status(201).json(contact);
 });
 
 //@desc GET contact
 //@route GET /api/contacts/:id
-//@access Public
+//@access private
 const getContact = asyncHanlder(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
-  if(!contact) {
+  if (!contact) {
     res.status(404);
     throw new Error("Contact not found");
   }
@@ -42,10 +43,10 @@ const getContact = asyncHanlder(async (req, res) => {
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//@access Public
+//@access private
 const updateContact = asyncHanlder(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
-  if(!contact) {
+  if (!contact) {
     res.status(404);
     throw new Error("Contact not found");
   }
@@ -53,7 +54,7 @@ const updateContact = asyncHanlder(async (req, res) => {
   const updateContact = await Contact.findByIdAndUpdate(
     req.params.id,
     req.body,
-    { new: true}
+    { new: true }
   );
 
   res.status(200).json(updateContact);
@@ -61,10 +62,10 @@ const updateContact = asyncHanlder(async (req, res) => {
 
 //@desc Create new contact
 //@route POST /api/contacts/:id
-//@access Public
+//@access private
 const deleteContact = asyncHanlder(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
-  if(!contact) {
+  if (!contact) {
     res.status(404);
     throw new Error("Contact not found");
   }
@@ -72,6 +73,10 @@ const deleteContact = asyncHanlder(async (req, res) => {
   res.status(200).json(contact);
 });
 
-module.exports = { getContacts, createContacts,  getContact, updateContact,  deleteContact };
-
-
+module.exports = {
+  getContacts,
+  createContacts,
+  getContact,
+  updateContact,
+  deleteContact,
+};
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getContact, createContacts, getContacts, updateContact, deleteContact } = require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenhandler");
 
-
+router.use(validateToken);
 router.route("/").get(getContacts).post(createContacts);
 router.route("/:id").put(updateContact).get(getContact).delete(deleteContact);
 
